15b82e8b334519c890aaff87aad2dad3cd87208c
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2024-05-22 17:02:03 +0100

    feat(color_picker): add more style attributes

--- gaphor/diagram/styleeditor.py
+++ gaphor/diagram/styleeditor.py
@@ -18,8 +18,10 @@ class StyleEditor:
         if self.window is None:
             self.window = self.window_builder.get_object("style-editor")
             self.window.connect("close-request", self.close)
-            self.color_button = self.window_builder.get_object("color-button")
-            self.color_button.connect("color-set", self.on_color_set)
+            self.window_builder.get_object("color").connect("color-set", self.on_color_set)
+            self.window_builder.get_object("border-radius").connect("value-changed", self.on_border_radius_set)
+            self.window_builder.get_object("background-color").connect("color-set", self.on_background_color_set)
+            self.window_builder.get_object("text-color").connect("color-set", self.on_text_color_set)
         self.window.present()
 
     def close(self, widget=None):
@@ -36,3 +38,26 @@ class StyleEditor:
         b = int(colors.blue * 255)
         a = colors.alpha
         self.subject.presentation_style.change_style("color", f"rgba({r}, {g}, {b}, {a})")
+
+    @transactional
+    def on_border_radius_set(self, widget):
+        self.subject.presentation_style.change_style("border-radius", widget.get_value())
+
+    @transactional
+    def on_background_color_set(self, widget):
+        colors = widget.get_rgba()
+        r = int(colors.red * 255)
+        g = int(colors.green * 255)
+        b = int(colors.blue * 255)
+        a = colors.alpha
+        self.subject.presentation_style.change_style("background-color", f"rgba({r}, {g}, {b}, {a})")
+
+    @transactional
+    def on_text_color_set(self, widget):
+        colors = widget.get_rgba()
+        r = int(colors.red * 255)
+        g = int(colors.green * 255)
+        b = int(colors.blue * 255)
+        a = colors.alpha
+        self.subject.presentation_style.change_style("text-color", f"rgba({r}, {g}, {b}, {a})")
+
