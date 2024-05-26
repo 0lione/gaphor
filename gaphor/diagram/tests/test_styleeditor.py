455333e61a7fcf92c9048a811944a8ab175d77f4
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2024-05-26 23:07:10 +0100

    test(color_picker): Guarantee that the created windows are not hanging after testing

--- gaphor/diagram/tests/test_styleeditor.py
+++ gaphor/diagram/tests/test_styleeditor.py
@@ -21,6 +21,7 @@ def test_style_editor_creation(diagram):
     background_color = find(style_editor.window, "background-color")
     text_color = find(style_editor.window, "text-color")
     assert color_picker and border_radius and background_color and text_color
+    style_editor.close()
 
 def test_set_border_radius(diagram):
     item = diagram.create(ClassItem)
@@ -30,6 +31,7 @@ def test_set_border_radius(diagram):
     border_radius = find(style_editor.window, "border-radius")
     border_radius.set_value(10)
     assert item.presentation_style.get_style("border-radius") == "10.0"
+    style_editor.close()
 
 def test_set_color(diagram):
     item = diagram.create(ClassItem)
@@ -42,6 +44,7 @@ def test_set_color(diagram):
     color_picker.set_rgba(rgba)
     style_editor.on_color_set(color_picker)
     assert item.presentation_style.get_style("color") == "rgba(255, 0, 0, 1.0)"
+    style_editor.close()
 
 def test_set_background_color(diagram):
     item = diagram.create(ClassItem)
@@ -54,6 +57,7 @@ def test_set_background_color(diagram):
     background_color.set_rgba(rgba)
     style_editor.on_background_color_set(background_color)
     assert item.presentation_style.get_style("background-color") == "rgba(0, 255, 0, 1.0)"
+    style_editor.close()
 
 def test_set_text_color(diagram):
     item = diagram.create(ClassItem)
@@ -66,6 +70,7 @@ def test_set_text_color(diagram):
     text_color.set_rgba(rgba)
     style_editor.on_text_color_set(text_color)
     assert item.presentation_style.get_style("text-color") == "rgba(0, 0, 255, 1.0)"
+    style_editor.close()
 
 def test_change_name(create, diagram, element_factory, event_manager):
     with Transaction(event_manager):
@@ -82,6 +87,7 @@ def test_change_name(create, diagram, element_factory, event_manager):
         assert item.presentation_style.get_style("color") == "rgba(0, 0, 255, 1.0)"
         item.subject.name = "New Name"
         assert item.presentation_style.get_style("color") == "rgba(0, 0, 255, 1.0)"
+        style_editor.close()
 
 def test_export(create, diagram, element_factory, event_manager):
     with Transaction(event_manager):
@@ -99,6 +105,7 @@ def test_export(create, diagram, element_factory, event_manager):
         style_editor.on_export(export)
         assert item.presentation_style.get_style("color") is None
         assert "rgba(0, 0, 255, 1.0)" in stylesheet.styleSheet
+        style_editor.close()
     
 def test_remove_item(create, diagram, element_factory, event_manager, modeling_language):
     page = DiagramPage(diagram, event_manager, modeling_language)
@@ -116,3 +123,4 @@ def test_remove_item(create, diagram, element_factory, event_manager, modeling_l
         style_editor.on_color_set(color)
         item.unlink()
         assert len(stylesheet.style_elems) == 0
+        style_editor.close()
